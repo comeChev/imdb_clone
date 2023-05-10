1c7cd03584ccf81641badb7763a6a8e15903d960
Côme Chevallier <come.chevallier@icloud.com>
2023-05-10 15:18:13 +0200

    adding about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 px-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all of your movie-related-needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news, reviews
+        and trailers. Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of cinemas. You can search for movies by title, director,
+        actor, genre or release date, making it easy to find the perfect movie
+        for any occasion.
+      </p>
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section dedicated to movie news and trailers,
+        keeping you updated with the latest happenings int the world of cinema.
+        Thank you for visiting our website and we hope you enjoy your time
+        browsing through our movie database. If you have any feedback or
+        suggestions, please feel free to contact us. We are always looking for
+        ways to improve our website and enhance the user experience on our
+        website. <span className="font-bold">Happy browsing ! 🤙</span>
+      </p>
+    </div>
+  );
+}
