032bb7e06d5b2b2b06e446c4adbde158dcf7638f
Côme Chevallier <come.chevallier@icloud.com>
2023-05-10 16:14:22 +0200

    creating fetch data form TMDB api on homePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
+import Results from "@/components/Results";
 import Image from "next/image";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const results = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return res.json();
+    })
+
+    .then((data) => data.results);
+
+  console.log(results);
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Results results={results} />
     </main>
   );
 }
